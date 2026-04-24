efa721d6a5e3c4df3ea40d49b2da8ad025c63a23
MatheusMachadoOliveira <matheusaulas2609@gmail.com>
2026-04-24 01:28:53 +0000

    aula 23/04

---
+++ login.js
@@ -0,0 +1,31 @@
+const email = "admin@exemplo.com"
+const senha = "1234"
+
+function verificarcredencias() {
+    const senhainformada = document.getElementById("senha").value;
+
+    if (emailinformado === email) {
+        alert("senha")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
